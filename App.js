61db6cedd7c01faa86c44ddd39b78bb96f77c7b8
Luissalamanca23 <l.salamanca18@icloud.com>
2024-04-03 09:41:32 -0300

    Integracion de Web Analytics

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Analytics } from "@vercel/analytics/react";
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Aplicación</h1>
+      {/* Otros componentes de tu aplicación */}
+      <Analytics id="prj_kuqm8FtSF9bJzoy4VPmNXMqOVAhy" />
+    </div>
+  );
+}
+
+export default App;
