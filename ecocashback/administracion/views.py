1a73d26a1502b2aa92aa0543793b313df85021da
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-23 19:09:27 -0400

    feat: Agregar vista y lógica para agregar eventos

--- ecocashback/administracion/views.py
+++ ecocashback/administracion/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Evento
+from .forms import EventoForm
 
-# Create your views here.
+def agregar_evento(request):
+    if request.method == 'POST':
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_eventos')  # Asegúrate de que 'listar_eventos' esté definido en tus URLs
+    else:
+        form = EventoForm()
+    return render(request, 'administracion/agregar_evento.html', {'form': form})
--- ecocashback/public/views.py
+++ ecocashback/public/views.py
@@ -1,5 +1,34 @@
-# public/views.py
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Newsletter
+from administracion.models import Evento
+from .forms import NewsletterForm
+import json
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'inicio.html')
+    eventos = Evento.objects.all()
+    return render(request, 'inicio.html', {'eventos': eventos})
+
+
+
+
+
+
+
+
+# eventos ecocashback 
+
+def listar_eventos(request):
+    eventos = Evento.objects.all()
+    return render(request, 'public/inicio.html', {'eventos': eventos})
+@csrf_exempt
+def agregar_suscripcion(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'public/inicio.html', {'eventos': Evento.objects.all(), 'message': 'Suscripción exitosa'})
+        else:
+            return render(request, 'public/inicio.html', {'eventos': Evento.objects.all(), 'message': 'Por favor, ingresa un email válido.'})
+    return redirect('listar_eventos')
