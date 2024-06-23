fae4ddc0348cf9cebafcbcdf30679a86e59b059b
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-23 18:59:43 -0400

    feat: Add models and admin registration for Comuna, Evento, and Region
    This commit adds the necessary models for Comuna, Evento, and Region in the administracion app. It also registers these models in the admin site for easy management.

--- ecocashback/administracion/admin.py
+++ ecocashback/administracion/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import (Comuna, Evento, Region)
 # Register your models here.
+admin.site.register(Comuna)
+admin.site.register(Evento)
+admin.site.register(Region)
--- ecocashback/administracion/models.py
+++ ecocashback/administracion/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Comuna(models.Model):
+    comuna_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    region = models.ForeignKey('Region', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Region(models.Model):
+    region_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Evento(models.Model):
+    evento_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField()
+    ubicacion = models.CharField(max_length=255)
+    comuna = models.ForeignKey('Comuna', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
