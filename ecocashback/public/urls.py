d2da30edcbe25e59229f39a1ca65fd6e1b175a34
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-23 19:00:20 -0400

    feat: Add URLs for listar_eventos and agregar_suscripcion views
    This commit adds the necessary URLs for the `listar_eventos` and `agregar_suscripcion` views in the `public.urls` module. These URLs will be used to route requests to the corresponding views for listing events and adding subscriptions.

--- ecocashback/public/urls.py
+++ ecocashback/public/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.listar_eventos, name='listar_eventos'),
+    path('suscribirse/', views.agregar_suscripcion, name='agregar_suscripcion'),
 ]
