d7a236c97a8e443e57efa812c448dd14765b3a33
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-22 21:12:16 -0400

    feat: Add public URLs and views for index page
    This commit adds the necessary code to create the URLs and views for the index page of the public section of the application. The `urls.py` file is created with a single URL pattern that maps to the `index` view in the `views.py` file. The `index` view is updated to render the `inicio.html` template instead of the `index.html` template.

---
+++ ecocashback/public/urls.py
@@ -0,0 +1,7 @@
+# public/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- ecocashback/public/views.py
+++ ecocashback/public/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render,redirect,get_object_or_404
+# public/views.py
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    contexto = {}
-    return render(request, 'index.html', contexto)
+    return render(request, 'inicio.html')
