77f8fc780aee1a89299a1133565a0ac76b9fe59a
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-24 14:35:32 -0400

    chore: Registrar modelos Newsletter, Usuario y Rol en el panel de administración

--- ecocashback/administracion/forms.py
+++ ecocashback/administracion/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Evento
+from .models import Usuario
 
 class EventoForm(forms.ModelForm):
     class Meta:
         model = Evento
         fields = ['nombre', 'descripcion', 'fecha', 'ubicacion', 'comuna']
+
+
+
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['nombre', 'email', 'contraseña', 'rol']
+        widgets = {
+            'contraseña': forms.PasswordInput(),  # Para que el campo de contraseña se renderice como un campo de contraseña
+        }
--- ecocashback/administracion/models.py
+++ ecocashback/administracion/models.py
@@ -26,3 +26,31 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Newsletter(models.Model):
+    newsletter_id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=100, unique=True)
+    usuario = models.ForeignKey('Usuario', on_delete=models.SET_NULL, null=True)
+    tipo_suscripcion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+
+
+class Usuario(models.Model):
+    usuario_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    contraseña = models.CharField(max_length=255)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    rol = models.ForeignKey('Rol', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Rol(models.Model):
+    rol_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
