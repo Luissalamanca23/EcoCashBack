af2514f344406a1b4e7b47f3b52af738ef25f86d
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-24 14:35:40 -0400

    chore: Actualizar configuración de registro de logs

--- ecocashback/ecocashback/settings.py
+++ ecocashback/ecocashback/settings.py
@@ -11,23 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import logging
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
