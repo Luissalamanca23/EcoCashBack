805ec8da5b4f964912a826f10e01c193dc5eda6f
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-25 17:48:49 -0400

    chore: Agregar formulario de eventos al archivo forms.py y actualizar rutas en urls.py

--- ecocashback/public/forms.py
+++ ecocashback/public/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from .models import Newsletter
+from .models import Evento
+
 
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
         fields = ['email', 'tipo_suscripcion']
+
+class EventoForm(forms.ModelForm):
+    class Meta:
+        model = Evento
+        fields = ['nombre', 'descripcion', 'fecha', 'ubicacion', 'comuna']
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Correo Electrónico', max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput(), label='Contraseña')
