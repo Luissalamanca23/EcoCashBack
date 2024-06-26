ad8775d963da7e64c3671832519f663b2c455a0a
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-06-25 23:01:53 -0400

    chore: Actualizar configuración de base de datos a PostgreSQL

--- ecocashback/ecocashback/settings.py
+++ ecocashback/ecocashback/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,18 @@ WSGI_APPLICATION = 'ecocashback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
+
 
 
 # Password validation
